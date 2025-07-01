3c4f57e5fdf92a4080374a8011236ff202fc6b9f
Yunpeng Hou <yunpenghou-macbookpro2023@Yunpengs-MacBook-Pro.local>
2025-06-30 21:25:00 -0700

    change some scrapy settings

--- python/crawler/redfin_spider/settings.py
+++ python/crawler/redfin_spider/settings.py
@@ -12,22 +12,24 @@ BOT_NAME = "redfin_spider"
 SPIDER_MODULES = ["redfin_spider.spiders"]
 NEWSPIDER_MODULE = "redfin_spider.spiders"
 
-ADDONS = {}
+ADDONS = {} # type: ignore
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "redfin_spider (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
