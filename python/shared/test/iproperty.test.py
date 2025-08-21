f897b6d9ccb1a96053ecd6ae550c0d6bc420852a
Yunpeng Hou <yunpenghou-macbookpro2023@Yunpengs-MacBook-Pro.local>
2025-08-20 22:52:47 -0700

    fix mypy errors

--- python/shared/test/iproperty.test.py
+++ python/shared/test/iproperty.test.py
@@ -1,12 +1,12 @@
 import unittest
-import usaddress # type: ignore
+import usaddress
 from datetime import datetime
 
 from shared.iproperty import IPropertyHistory, IPropertyHistoryEvent, PropertyHistoryEventType
 from shared.iproperty_address import IPropertyAddress, get_address_components
 
 class Test_get_address(unittest.TestCase):
-    def test_street_address(self):
+    def test_street_address(self) -> None:
         # Map from full streed address to full street address
         testCases = {
             "1838 Market St,Kirkland, WA 98033" : "1838 Market St",
@@ -21,7 +21,7 @@ class Test_get_address(unittest.TestCase):
             streetAddress = components["street"]
             self.assertEqual(expectedStreetAddress, streetAddress)
     
-    def test_unit(self):
+    def test_unit(self) -> None:
         # Map from full streed address to full street address
         testCases = {
             "1838 Market St,Kirkland, WA 98033" : "",
@@ -35,15 +35,15 @@ class Test_get_address(unittest.TestCase):
             self.assertEqual(expectedStreetAddress, unit)
 
 class Test_IPropertyHistory(unittest.TestCase):
-    def test_history_events(self):
+    def test_history_events(self) -> None:
         address = IPropertyAddress("1838 Market St,Kirkland, WA 98033")
         events = [
-            IPropertyHistoryEvent(datetime(2022, 1, 1), PropertyHistoryEventType.Listed, "Listed", 1000000),
-            IPropertyHistoryEvent(datetime(2022, 3, 1), PropertyHistoryEventType.Sold, "Sold", 940000),
+            IPropertyHistoryEvent("event1", datetime(2022, 1, 1), PropertyHistoryEventType.Listed, "Listed", price=1000000),
+            IPropertyHistoryEvent("event2", datetime(2022, 3, 1), PropertyHistoryEventType.Sold, "Sold", price=940000),
         ]
         history = IPropertyHistory("test-id", address, events)
         self.assertEqual(len(history._history), 2)
-        history.addEvent(IPropertyHistoryEvent(datetime(2022, 2, 1), PropertyHistoryEventType.PriceChange, "Price dropped", 950000))
+        history.addEvent(IPropertyHistoryEvent("event3", datetime(2022, 2, 1), PropertyHistoryEventType.PriceChange, "Price dropped", price=950000))
         self.assertEqual(len(history._history), 3)
 
         self.assertEqual(history._history[2]._event_type, PropertyHistoryEventType.Sold)
